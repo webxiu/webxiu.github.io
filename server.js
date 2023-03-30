795f5898143a2d35604046bb1f2f4228ec960365
Hailen <759430324@qq.com>
2023-03-30 13:01:56 +0800

    fix error

---
+++ server.js
@@ -0,0 +1,15 @@
+require('colors')
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+const port = 3000
+app.listen(port, () => {
+    const title = '\n- server:'.rainbow;
+    const address = `http://localhost:${port}`.blue;
+    console.log(title + address)
+})
