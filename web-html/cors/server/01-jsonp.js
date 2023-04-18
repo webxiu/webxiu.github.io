5eb583eff0db092a68ba3b34bd0c3e16b1c1f8b1
Hailen <759430324@qq.com>
2023-04-18 15:42:34 +0800

    fix

--- web-html/cors/server/01-jsonp.js
+++ web-html/cors/server/01-jsonp.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express()
 
+// https://www.bilibili.com/video/BV1wT4y1g788/?p=1&vd_source=0ff4a7894b0b330a6668a7b782cf0df7
+
 app.get('/jsonp', (req, res) => {
     const { callback = Function.prototype } = req.query;
     const data = { code: 0, msg: '成功' }
