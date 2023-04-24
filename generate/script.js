a8912a79c401cae1fd528ea0068c445347d8eaeb
Hailen <759430324@qq.com>
2023-04-24 21:32:51 +0800

    添加项目

--- generate/script.js
+++ generate/script.js
@@ -17,4 +17,20 @@ function createWebHtml() {
   const dataStr = JSON.stringify(links).replace(new RegExp(path.join(__dirname, '../web-html'), 'g'), '.')
   tools.writeFile(outPath, template.getGameTemplate(dataStr))
 }
-createWebHtml()
+// createWebHtml()
+
+// console.log('__dirname', path.join(__dirname, '../web-html')) // xxxx/webxiu.github.io/generate
+function createWebHtml2() {
+  const inputPath = path.join(__dirname, '../project');
+  const outPath = path.join(__dirname, '../project/index.html');
+
+  const links = tools.getDirPaths({
+    rootPath: inputPath,
+    exclude: ['images', 'css', 'js', 'img', 'assets'],
+    include: ['.html']
+  });
+
+  const dataStr = JSON.stringify(links).replace(new RegExp(path.join(__dirname, '../project'), 'g'), '.')
+  tools.writeFile(outPath, template.getProjectTemplate(dataStr))
+}
+createWebHtml2()
